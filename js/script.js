45bec87b8d288cfd5f289d6084e374eea8c5baef
Deejai <burmaud1@gmail.com>
2020-07-29 13:34:21 -0400

    finished with the T-Shirt info

--- js/script.js
+++ js/script.js
@@ -1,93 +1,85 @@
 //setting name on focus
-const nameInputField = document.getElementById('name');
+const nameInputField = document.getElementById("name");
 nameInputField.focus();
 
 //get all children of the select so that i can lis
-const seleted = document.querySelector('select');
+const seleted = document.querySelector("select");
 //console.log(seleted);
 
 //get the input field to hide first
-const otherInputTextField = document.getElementById('other-title');
-otherInputTextField.style.display = 'none';
+const otherInputTextField = document.getElementById("other-title");
+otherInputTextField.style.display = "none";
 //hiding the other textfield based on
 //whether or not 'other' was clicked
-seleted.addEventListener('change', (e) => {
-    // console.log(e.target.value);
-    const selecter = e.target.value;
-    if(selecter === 'other'){
-        otherInputTextField.style.display = 'block';
-    }else{
-        otherInputTextField.style.display = 'none';
-    }
+seleted.addEventListener("change", (e) => {
+  // console.log(e.target.value);
+  const selecter = e.target.value;
+  if (selecter === "other") {
+    otherInputTextField.style.display = "block";
+  } else {
+    otherInputTextField.style.display = "none";
+  }
 });
 
 //i have to grab the the design and color element
-const designElement = document.getElementById('design');//design
+const designElement = document.getElementById("design"); //design
 //console.log(designElement.value);
 
-const colorElement = document.getElementById('color');//colour
+const colorElement = document.getElementById("color"); //colour
 //console.log(colorElement);
 
 //i think i shold make another select option for colorElement
-const before = document.createElement('option');
-before.innerHTML = 'Please select A T-shirt theme';
+const before = document.createElement("option");
+before.innerHTML = "Please select A T-shirt theme";
 before.value = "Tshirt theme";
 colorElement.appendChild(before);
-
 
 var hideSelect = document.querySelector('option[value = "Tshirt theme"]');
 hideSelect.hidden = true;
 console.log(hideSelect);
 //default
-if(designElement.value === 'Select Theme'){
-  colorElement.value = before.value;//Please select a t-shirt theme
-};
+if (designElement.value === "Select Theme") {
+  colorElement.value = before.value; //Please select a t-shirt theme
+}
+//get the please select at Theme
 
-//get the please select a t Theme
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+designElement.addEventListener("change", (e) => {
+  const target = e.target.value;
+  console.log(target);
+  const selectall = document.querySelector("#color");
+  if (target === "js puns") {
+    for (let i = 0; i < selectall.length; i++) {
+      selectall[i].hidden = false;
+      //console.log(selectall[i].value);
+      if (
+        selectall[i].value === "cornflowerblue" ||
+        selectall[i].value === "darkslategrey" ||
+        selectall[i].value === "gold"
+      ) {
+        selectall[i].className = "match";
+      } else {
+        selectall[i].hidden = true;
+      }
+    }
+  } else {
+    for (let i = 0; i < selectall.length; i++) {
+      //console.log(selectall[i].value);
+      selectall[i].hidden = false; //make all of them visible for the moment
+      if (
+        selectall[i].value === "tomato" ||
+        selectall[i].value === "steelblue" ||
+        selectall[i].value === "dimgrey"
+      ) {
+        selectall[i].className = "match";
+      } else {
+        selectall[i].hidden = true;
+      }
+    }
+  }
+});
 
 //array for matching
 //let matching = [];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //the addEventListener for design
 // designElement.addEventListener('change', (e) => {
 //   let changedDesign = e.target.value;
